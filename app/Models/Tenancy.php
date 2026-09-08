459f30b81cd06b68ead8e0d95571f0c419429ef3
Stevenc20 <stevenchristian2005@gmail.com>
2026-09-08 21:01:30 +0700

    Add user and property relationships to Tenancy model

--- app/Models/Tenancy.php
+++ app/Models/Tenancy.php
@@ -4,7 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Tenancy extends Model
 {
-    //
+    /**
+     * Get the user associated with the tenancy.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the property associated with the tenancy.
+     */
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class);
+    }
 }
