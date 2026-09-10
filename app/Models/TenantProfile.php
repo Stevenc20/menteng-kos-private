308c7634ae4ec3226248f694999f3683cb3e6c8a
Stevenc20 <stevenchristian2005@gmail.com>
2026-09-10 18:30:38 +0700

    Add fillable to TenantProfile so storeInfo/save actually persists data

--- app/Models/TenantProfile.php
+++ app/Models/TenantProfile.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class TenantProfile extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'whatsapp',
+        'ktp_1_photo', 'ktp_1_name', 'ktp_1_nik', 'ktp_1_birth_place', 'ktp_1_birth_date', 'ktp_1_job', 'ktp_1_address',
+        'ktp_2_photo', 'ktp_2_name', 'ktp_2_nik', 'ktp_2_birth_place', 'ktp_2_birth_date', 'ktp_2_job', 'ktp_2_address',
+    ];
 }
