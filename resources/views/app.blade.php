68d5fb43ae07e7e5e8570b222a6ebc48190a38f9
Stevenc20 <stevenchristian2005@gmail.com>
2026-09-11 12:59:36 +0700

    fix(favicon): ganti URL favicon ke /favicon.png agar browser tidak pakai cache favicon lama

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,8 +31,8 @@
             }
         </style>
 
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32">
         <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         @fonts
